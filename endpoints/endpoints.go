bc7a5ec912f9a62676ec44e5d9fb4469ba4c932c
Curtis La Graff <curtis@lagraff.me>
2018-03-02 17:08:28 -0500

    remove config stuff for now
    Signed-off-by: Curtis La Graff <curtis@lagraff.me>

---
+++ endpoints/endpoints.go
@@ -0,0 +1,77 @@
+package endpoints
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/nbari/violetear"
+)
+
+func copyRequest(original *http.Request) (http.Request, error) {
+	copy := *original
+
+	// Use new URL Userinfo struct instead of the original pointer
+	if original.URL.User != nil {
+		// TODO: does this actually work?
+		copy.URL.User = &url.Userinfo{}
+		(*copy.URL.User) = *original.URL.User
+	}
+
+	buf, err := ioutil.ReadAll(original.Body)
+	if err != nil {
+		return copy, err
+	}
+
+	// User new Body and GetBody instead of original ones.
+	copy.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
+
+	origBody := ioutil.NopCloser(bytes.NewBuffer(buf))
+	original.Body = origBody
+	original.GetBody = func() (io.ReadCloser, error) {
+		return original.Body, nil
+	}
+
+	// We dont support the copying of this information.
+	copy.MultipartForm = nil
+	copy.TLS = nil
+	copy.Response = nil
+
+	return copy, nil
+}
+
+func createCatchAllRoute(receivedRequests chan http.Request) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		copiedReq, err := copyRequest(r)
+		if err != nil {
+			panic(err)
+		}
+
+		receivedRequests <- copiedReq
+		w.Write([]byte(""))
+	}
+}
+
+func CreateServer(receivedRequests chan http.Request, port int) (*http.Server, func() error) {
+	listenAddr := fmt.Sprintf(":%d", port)
+
+	router := violetear.New()
+	router.LogRequests = false
+	router.Verbose = false
+
+	router.HandleFunc("*", createCatchAllRoute(receivedRequests))
+
+	srv := &http.Server{
+		Addr:           listenAddr,
+		Handler:        router,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   7 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	return srv, srv.ListenAndServe
+}
--- main.go
+++ main.go
@@ -1,140 +1,43 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 
-	"github.com/nbari/violetear"
-	"github.com/olebedev/config"
+	"github.com/clagraff/pobox/endpoints"
 )
 
-var defaultConfigJSON string = `{
-	"port": 8080,
-	"verbose": false
-}`
-
-var help *bool = flag.Bool("h", false, "Show help text")
-var configPath *string = flag.String("cfg", "", "Path to configuration JSON or Yaml.")
-var appConfig *config.Config
-
-func init() {
-	flag.Parse()
-
-	if help != nil && *help {
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
-
-	var err error
-	appConfig, err = config.ParseJson(defaultConfigJSON)
-	if err != nil {
-		fmt.Println("failed to load default configuration settings")
-		panic(err)
-	}
-
-	if configPath != nil && len(*configPath) > 0 {
-		parts := strings.Split(*configPath, ".")
-		ext := parts[len(parts)-1]
-
-		if ext == "json" {
-			appConfig, err = config.ParseJsonFile(*configPath)
-
-		} else if ext == "yml" || ext == "yaml" {
-			appConfig, err = config.ParseYamlFile(*configPath)
-		} else {
-			fmt.Println("invalid configuration file extension. must be JSON or Yaml")
-			os.Exit(-1)
-		}
-
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-type ParsedRequest struct {
-	Method    string
-	Headers   map[string]string
-	Body      []byte
-	URL       string
-	Timestamp time.Time
-}
-
-func MakeParsedRequest(req http.Request) ParsedRequest {
-	r := ParsedRequest{
-		Method:    req.Method,
-		URL:       req.URL.String(),
-		Headers:   make(map[string]string),
-		Timestamp: time.Now().UTC(),
-	}
-
-	for key, value := range req.Header {
-		r.Headers[key] = strings.Join(value, "")
-	}
-
-	bytes, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		panic(err)
-	}
-	r.Body = bytes
-
-	return r
-}
-
-var incomingRequests chan ParsedRequest = make(chan ParsedRequest)
-
-func catchAll(w http.ResponseWriter, r *http.Request) {
-	incomingRequests <- MakeParsedRequest(*r)
-	w.Write([]byte(""))
-}
-
-func runRequestServer() {
-	router := violetear.New()
-	router.LogRequests = false
-	router.Verbose = false
-
-	router.HandleFunc("*", catchAll)
-
-	srv := &http.Server{
-		Addr:           fmt.Sprintf(":%s", appConfig.UString("port", "8080")),
-		Handler:        router,
-		ReadTimeout:    5 * time.Second,
-		WriteTimeout:   7 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	if appConfig.UBool("verbose", false) {
-		fmt.Printf("running server on %s\n", appConfig.UString("port", "8080"))
-	}
-
-	srv.ListenAndServe()
-}
-
-func logRequests() {
+func logRequests(receivedRequests chan http.Request) {
 	for {
 		select {
-		case r := <-incomingRequests:
-			fmt.Printf("UTC Timestamp:\t\t%s\n", r.Timestamp)
-			fmt.Printf("Local Timestamp:\t%s\n", r.Timestamp.Local())
+		case r := <-receivedRequests:
+			fmt.Printf("UTC Time:\t%s\n", time.Now().UTC())
+			fmt.Printf("Local Time:\t%s\n", time.Now().Local())
 			fmt.Printf("Method:\t%s\n", r.Method)
 			fmt.Printf("URL:\t%s\n", r.URL)
 			fmt.Println("Headers:")
-			for key, value := range r.Headers {
-				fmt.Printf("\t%s: %s\n", key, value)
+			for key, value := range r.Header {
+				fmt.Printf("\t%s: %s\n", key, strings.Join(value, ""))
 			}
 			fmt.Println("Body:")
-			fmt.Printf("\t%s\n", r.Body)
+
+			buf, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("\t%s\n", string(buf))
 		default:
 		}
 	}
 }
 
 func main() {
-	go func() { runRequestServer() }()
-	logRequests()
+	rr := make(chan http.Request)
+	_, start := endpoints.CreateServer(rr, 8080)
+	go func() { start() }()
+
+	logRequests(rr)
 }
